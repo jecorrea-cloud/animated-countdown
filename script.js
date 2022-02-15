7eca5c2456dc232223d94d5b6408088bf78191bb
joaquin <jecorrea@protonmail.com>
2022-02-15 18:57:22 -0500

    initial JS

---
+++ script.js
@@ -0,0 +1,6 @@
+// Bring down the elements
+
+const nums = document.querySelectorAll(".nums span");
+const counter = document.querySelector(".counter");
+const finalMessage = document.querySelector(".final");
+const replay = document.querySelector("#replay");
